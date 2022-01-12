0508d3179e784bafa4f46ef8044ba266e78cc3ed
Nicholas Nolen <nicnolen@ymail.com>
2022-01-12 10:27:23 -0600

    wrote failing tests for enemy health, is alive, attack, and reduce health

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -16,3 +16,46 @@ test('creates an emey object', () => {
   expect(enemy.strength).toEqual(expect.any(Number));
   expect(enemy.agility).toEqual(expect.any(Number));
 });
+
+// Test for enemy health
+test("gets enemy's health value", () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.getHealth()).toEqual(
+    expect.stringContaining(enemy.health.toString())
+  );
+});
+
+// Test if enemy is still alive
+test('checks if enemy is alive or not', () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.isAlive()).toBeTruthy();
+
+  enemy.health = 0;
+
+  expect(enemy.isAlive()).toBeFalsy();
+});
+
+// Test for enemy attack value
+test("gets enemy's attack value", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  enemy.strength = 10;
+
+  expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+// Test for subtracting enemy health
+test("subtracts from enemy's health", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  const oldHealth = enemy.health;
+
+  enemy.reduceHealth(5);
+
+  expect(enemy.health).toBe(oldHealth - 5);
+
+  enemy.reduceHealth(99999);
+
+  expect(enemy.health).toBe(0);
+});
